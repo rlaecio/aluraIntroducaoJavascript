a3e3ee348041ea7bef3cbe63baa95f305754e37f
Gitlab.network <roque.souza@portodigital.pt>
2020-12-26 20:20:55 +0000

    validando formularios

--- js/calculaImc.js
+++ js/calculaImc.js
@@ -13,27 +13,49 @@ for (var i = 0; i < pacientes.length; i++) {
     var tdAltura = paciente.querySelector(".info-altura");
     var altura = tdAltura.textContent;
     var tdImc = paciente.querySelector(".info-imc");
-    var pesoEhValido = true;
-    var AlturaEhValida = true;
 
-    if (peso <= 0 || peso >= 1000) {
+    var pesoEhValido = validaPeso(peso);
+    var AlturaEhValida = validaAltura(altura);
+
+    if(!pesoEhValido){
         pesoEhValido = false;
         tdImc.textContent = "Peso inválido!";
         paciente.classList.add("paciente-invalido");
     }
-    if (altura <= 0 || altura >= 3.00) {
+
+    if(!AlturaEhValida){
         AlturaEhValida = false;
         tdImc.textContent = "Altura inválida";
         paciente.classList.add("paciente-invalido");
     }
+
     if (AlturaEhValida && pesoEhValido) {
         var imc = calculaImc(peso, altura);
         tdImc.textContent = imc;
     }
+
 }
+
+function validaPeso(peso) {
+    if (peso >= 0 && peso < 1000) {
+       return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+    if (altura >= 0 && altura <= 3.00) { 
+        return true;
+    } else {
+        return false;
+    }
+}   
 
 function calculaImc(peso, altura) {
     var imc = 0;
     imc = peso / (altura * altura);
     return imc.toFixed(2)
 }
+
+
--- js/form.js
+++ js/form.js
@@ -8,16 +8,21 @@ botaoAdicionar.addEventListener("click", function(event){
     // extrai do formulario as informaçaos do paciente
     var paciente = obtemPacienteDoFormulario(form);
     var pacienteTr = montaTr(paciente);
+
+    var erros = validaPaciente(paciente);
+    if (erros.length > 0) {
+        var mensagemErro = document.querySelector("#mensagem-erro");
+        mensagemErro.textContent = erros;
+        return;
+    }
     
     // adiciona o paciente a tabela
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
 
     //limpando o formulario
-    form.reset();
-    
+    form.reset(); 
 });
-
 
 function obtemPacienteDoFormulario(form) {
     var paciente = {
@@ -50,4 +55,12 @@ function montaTd(dado, classe) {
     td.classList.add(classe);
 
     return td
+}
+
+function validaPaciente(paciente) {
+    var erros = [];
+    if(!validaPeso(paciente.peso)) erros.push("O peso é inválido");
+    if(!validaAltura(paciente.altura)) erros.push("A altura é inválida");
+    
+    return erros;
 }
