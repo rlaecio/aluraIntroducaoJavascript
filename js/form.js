520f18b44a07b3d220ddc4345a40121415adfea7
Gitlab.network <roque.souza@portodigital.pt>
2020-12-26 18:44:55 +0000

    Update form.js

--- js/form.js
+++ js/form.js
@@ -4,14 +4,17 @@ botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
 
     var form = document.querySelector("#form-adiciona");
+    
     // extrai do formulario as informaçaos do paciente
     var paciente = obtemPacienteDoFormulario(form);
-   
     var pacienteTr = montaTr(paciente);
     
     // adiciona o paciente a tabela
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
+
+    //limpando o formulario
+    form.reset();
     
 });
 
@@ -31,26 +34,12 @@ function montaTr(paciente) {
     var pacienteTr = document.createElement("tr"); // inicializa tr
     pacienteTr.classList.add("paciente");
 
-    // inicializa os td's 
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    // atribui valores do form aos td's
-    var nomeTd = montaTd(paciente.nome, "info-nome");
-    var pesoTd = montaTd(paciente.peso, "info-peso");
-    var alturaTd = montaTd(paciente.altura, "info-altura");
-    var gorduraTd = montaTd(paciente.gordura, "info-gordura");
-    var imcTd = montaTd(paciente.imc, "info-imc");
-    
     //cria a tabela final
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    pacienteTr.appendChild(montaTd(paciente.nome, "info-nome"));
+    pacienteTr.appendChild(montaTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(montaTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(montaTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(montaTd(paciente.imc, "info-imc"));
 
     return pacienteTr;
 }
